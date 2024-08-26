22792b23f613bc64fb55ea8bda4f4d196ad3bf34
ashish0038 <ashish.0038@gmail.com>
2024-08-26 13:43:54 +0530

    mobile page fixes

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -144,6 +144,58 @@ $('.kidsLesson').slick({
     // instead of a settings object
   ]
 });
+
+
+$('.kidsPaiting').slick({
+  centerMode: false,
+  centerPadding: '0px',
+  dots: false,
+  infinite: true,
+  prevArrow: '<button type="button" class="slick-prev"> <i data-feather="arrow-left"></i> </button>',
+  nextArrow: '<button type="button" class="slick-next"><i data-feather="arrow-right"></i></button>',
+  speed: 300,
+  arrows: true,
+  autoplay: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
 //eventsLesson
 $('.eventsLesson').slick({
   centerMode: false,
