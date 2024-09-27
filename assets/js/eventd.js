87d713aebfc026e5891af42713060c5dd3882a85
ashish0038 <ashish.0038@gmail.com>
2024-09-27 20:11:06 +0530

    removed video and added tag in images

--- assets/js/eventd.js
+++ assets/js/eventd.js
@@ -1,426 +1,430 @@
 var c_eman = [{
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/1.JPG',
 },
 {
     id: '2',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/2.JPG',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/3.JPG',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/4.JPG',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/5.JPG',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/6.png',
 },
 {
     id: '1',
-    name: 'name',
-    imageName: 'assets/img/events/18thevents/7.mp4',
-},
-{
-    id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/8.JPG',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/9.JPG',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/10.JPG',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/11.JPG',
 },
 {
     id: '1',
-    name: 'name',
-    imageName: 'assets/img/events/18thevents/12.mp4',
-},
-{
-    id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/13.JPG',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/14.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/15.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/16.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/17.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/18.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/19.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/20.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/21.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/23.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/24.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/25.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/29.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/30.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/31.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/32.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/34.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/36.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/37.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/38.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/39.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/40.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/41.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/42.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/44.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/47.mp4',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/49.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/52.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/53.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/55.jpeg',
 }
     , {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/56.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/57.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/58.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/59.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/60.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/62.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/63.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/64.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/65.jpeg',
 }
     ,
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/66.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/68.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/69.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/71.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/72.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/73.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/74.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/75.jpeg',
 }
     ,
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/76.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/77.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/78.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/79.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/80.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/22.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/27.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/28.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/33.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/35.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/43.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/45.jpeg',
 },
 {
     id: '1',
-    name: 'name',
-    imageName: 'assets/img/events/18thevents/48.mp4',
-},
-{
-    id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/50.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/51.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/54.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/67.jpeg',
 },
 {
     id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/70.jpeg',
 },
 {
     id: '1',
-    name: 'name',
-    imageName: 'assets/img/events/18thevents/82.mp4',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
+    imageName: 'assets/img/events/18thevents/26.jpeg',
+}
+];
+
+/*
+{
+    id: '1',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
+    imageName: 'assets/img/events/18thevents/7.mp4',
 },
 {
     id: '1',
-    name: 'name',
-    imageName: 'assets/img/events/18thevents/83.mp4',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
+    imageName: 'assets/img/events/18thevents/12.mp4',
 },
 {
     id: '1',
-    name: 'name',
-    imageName: 'assets/img/events/18thevents/84.mp4',
-},
-{
-    id: '1',
-    name: 'name',
-    imageName: 'assets/img/events/18thevents/85.mp4',
-},
-{
-    id: '1',
-    name: 'name',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
     imageName: 'assets/img/events/18thevents/86.mp4',
 },
 {
     id: '1',
-    name: 'name',
-    imageName: 'assets/img/events/18thevents/26.jpeg',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
+    imageName: 'assets/img/events/18thevents/82.mp4',
 },
 {
     id: '1',
-    name: 'name',
-    imageName: 'assets/img/events/18thevents/46.mp4',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
+    imageName: 'assets/img/events/18thevents/83.mp4',
+},
+{
+    id: '1',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
+    imageName: 'assets/img/events/18thevents/84.mp4',
+},
+{
+    id: '1',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
+    imageName: 'assets/img/events/18thevents/85.mp4',
 }
-];
+{
+    id: '1',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
+    imageName: 'assets/img/events/18thevents/46.mp4',
+},
+{
+    id: '1',
+    name: '18वें वार्षिकोत्सव में श्री प्राणनाथ जी वाणी स्टॉल',
+    imageName: 'assets/img/events/18thevents/48.mp4',
+} 
+    
+*/
 
 function displayTeam(divid, teamName) {
     const studentsList = document.getElementById(divid);
@@ -434,8 +438,9 @@ function displayTeam(divid, teamName) {
                 `
                 <div class="col-12 col-sm-4 col-md-4 mb-4">
                     <div class="image--wrapper">
+                        <span class="image_tag">${teamName[i].name}</span>
                         <div class="reveal reveal--overlay reveal--top">
-                            <video autoplay muted class="w-100" alt="" preload="metadata">
+                            <video autoplay muted class="w-100" alt="" preload="none">
                                 <source src="${teamName[i].imageName}"  type="video/mp4">
                             </video>
                         </div>
@@ -448,6 +453,7 @@ function displayTeam(divid, teamName) {
                 `
                 <div class="col-12 col-sm-4 col-md-4 mb-4">
                     <div class="image--wrapper">
+                    <span class="image_tag">${teamName[i].name}</span>
                         <div class="reveal reveal--overlay reveal--top">
                             <img src="${teamName[i].imageName}" class="w-100" alt="">
                         </div>
