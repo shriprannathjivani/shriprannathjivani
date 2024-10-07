5abc238de4e87e7f792e79a389aa25aa33a9cc71
ashish0038 <ashish.0038@gmail.com>
2024-10-07 16:02:45 +0530

    added sewa page

---
+++ assets/js/karkhanaNames.js
@@ -0,0 +1,222 @@
+
+var karkhanaList = [
+    {
+        id: "1",
+        imgLink: "assets/img/events/18thevents/4.JPG",
+        service: 'Paintings Seva Karkhana',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "2",
+        imgLink: "assets/img/events/18thevents/2.JPG",
+        service: 'Typing, Translation and Proofreading Seva Karkhana ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "3",
+        imgLink: "assets/img/events/18thevents/3.JPG",
+        service: 'Quizzes Seva Karkhana',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "4",
+        imgLink: "assets/img/events/18thevents/5.JPG",
+        service: 'Chaupai Decor Seva Karkhana ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "5",
+        imgLink: "assets/img/events/18thevents/11.JPG",
+        service: 'Paintings Seva Karkhana ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "6",
+        imgLink: "assets/img/events/18thevents/8.JPG",
+        service: 'Vani Gayan Seva Karkhana ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "7",
+        imgLink: "assets/img/events/18thevents/9.JPG",
+        service: 'Reels/Shorts Seva Karkhana  ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "8",
+        imgLink: "assets/img/events/18thevents/10.JPG",
+        service: 'Coordinators Seva Karkhana ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "9",
+        imgLink: "assets/img/events/18thevents/11.JPG",
+        service: 'Reminder Seva Karkhana ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "10",
+        imgLink: "assets/img/events/18thevents/25.jpeg",
+        service: ' Sehyogita Seva Karkhana',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "11",
+        imgLink: "assets/img/events/18thevents/24.jpeg",
+        service: 'Video production Seva Karkhana',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "12",
+        imgLink: "assets/img/events/18thevents/14.jpeg",
+        service: 'Thumbnails Seva Karkhana',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "13",
+        imgLink: "assets/img/events/18thevents/15.jpeg",
+        service: 'Meethi Batiyaan Seva Karkhana',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "14",
+        imgLink: "assets/img/events/18thevents/16.jpeg",
+        service: 'Aatmik Reminders Seva Karkhana ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "15",
+        imgLink: "assets/img/events/18thevents/17.jpeg",
+        service: 'Notice Board Seva Karkhana ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "16",
+        imgLink: "assets/img/events/18thevents/18.jpeg",
+        service: 'Designing and Execution Seva Karkhana',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "17",
+        imgLink: "assets/img/events/18thevents/19.jpeg",
+        service: 'Desh Videsh se sath ji ke bhav  ',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "18",
+        imgLink: "assets/img/events/18thevents/20.jpeg",
+        service: 'Baal kendra Seva Karkhana',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    },
+    {
+        id: "19",
+        imgLink: "assets/img/events/18thevents/21.jpeg",
+        service: 'Writers Seva Karkhana',
+        description: 'As a Custom Paintings and Artwork Specialist, you will be responsible for creating stunning and personalized paintings and artwork for our clients. You will collaborate closely with clients to understand their vision and preferences, and then use your artistic skills to bring their ideas to life.',
+        contactPerson: 'Kamlesh Patel',
+        contactNo: '+91 0000 0000',
+        contactEmail: 'shriprannathjivani@gmail.com',
+        link: ""
+    }
+]
+
+
+function displaykarkhanaNames(divid, teamName) {
+    const studentsList = document.getElementById(divid);
+    for (let i = 0; i < teamName.length; i++) {
+
+        studentsList.innerHTML =
+            studentsList.innerHTML +
+            `
+                <div class="col-lg-3 col-md-3 col-sm-12 mb-5">
+                    <div class="card karkhanaListCard">
+                        <img src="${teamName[i].imgLink}" class="card-img-top" >
+                        <div class="card-body">
+                            <h5 class="card-title">${teamName[i].service}</h5>
+                            <p class="card-text-desc">${teamName[i].description}</p>
+                            <div class="contact-detail">
+                                <span class="mb-2 d-flex">Contact us: </span>
+                                <p class="card-text">${teamName[i].contactPerson}</p>
+                                <p class="card-text">${teamName[i].contactNo}</p>
+                                 <p class="card-text">${teamName[i].contactEmail}</p>
+                            </div>
+                            <a href="${teamName[i].link}" class="btn btn-primary btnapply">Apply</a>
+                        </div>
+                    </div>
+                 </div>
+                `;
+    }
+}
+displaykarkhanaNames("karkhanaList", karkhanaList);
