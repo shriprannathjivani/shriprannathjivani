cce06d9bcb8bb252f566b3543e8707aadda16420
ashish0038 <ashish.0038@gmail.com>
2025-06-17 14:04:32 +0530

    quick link added home page

---
+++ assets/js/quicklinks.js
@@ -0,0 +1,77 @@
+
+var quickLink = [
+    {
+        id: "1",
+        name: 'आत्मदर्शनम्',
+        link: "atmadarshanam.html",
+        img: "eye"
+    }, {
+        id: "2",
+        name: 'खेल-खेल में ज्ञान',
+        link: "atmadarshanam_games.html",
+        img: "user"
+    }, {
+        id: "3",
+        name: 'ज्ञानपीठ वाणी साहित्य',
+        link: "https://drive.google.com/drive/folders/1gLm214M5g71RT397XsWfCxHbCyoNxqKv",
+        img: "music"
+    }, {
+        id: "4",
+        name: 'पुस्तकालय',
+        link: "sahitya.html",
+        img: "book-open"
+    }, {
+        id: "5",
+        name: 'ऑडियो',
+        link: "audio.html",
+        img: "headphones"
+    }, {
+        id: "6",
+        name: 'वीडियो',
+        link: "video.html",
+        img: "video"
+    },{
+        id: "7",
+        name: 'ई-मंथन लेखन',
+        link: "articles.html",
+        img: "pen-tool"
+    },{
+        id: "8",
+        name: 'शिशु नामकरण',
+        link: "babynames.html",
+        img: "users"
+    },{
+        id: "9",
+        name: 'सेवा कारखाना (सेवा केंद्र)',
+        link: "SevaKarkhana.html",
+        img: "user-plus"
+    },{
+        id: "10",
+        name: 'गूगल क्विज़',
+        link: "quizzes.html",
+        img: "help-circle"
+    },{
+        id: "11",
+        name: 'मासिक कैलेंडर (डाउनलोड)',
+        link: "monthlyCalendar.html",
+        img: "calendar"
+    },
+]
+
+
+function displaysahiyogitaNames(divid, quickLinkName) {
+    const studentsList = document.getElementById(divid);
+    for (let i = 0; i < quickLinkName.length; i++) {
+
+        studentsList.innerHTML =
+            studentsList.innerHTML +
+            `
+                    <a target="_blank" href="${quickLinkName[i].link}" class="quicklinkItem">
+                        <i data-feather="${quickLinkName[i].img}"></i>
+                        ${quickLinkName[i].name}
+                    </a>
+                `;
+    }
+}
+displaysahiyogitaNames("quickLink", quickLink);
+
