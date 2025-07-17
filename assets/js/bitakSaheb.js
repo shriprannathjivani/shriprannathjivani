41c31f39d8de9e02ccf5cab174e293f44fc7544c
ashish0038 <ashish.0038@gmail.com>
2025-07-17 17:39:39 +0530

    updated bitak saheb day 3

--- assets/js/bitakSaheb.js
+++ assets/js/bitakSaheb.js
@@ -2,6 +2,40 @@
 var bitakSahebList = [
     {
         id: 1,
+        date: { day: "14", month: "July", year: "2025" },
+        title: "श्री प्राणनाथ जी वाणी ज़ूम (प्रेम मंदिर) में पूज्य स्वामी जी के पधारने पर सभी सुंदरसाथ जी में उत्साह की लहर",
+        speaker: "श्री राजन स्वामी जी",
+        time: "6:00-7:30 am",
+        description: "पुलकित हृदयों से श्री बीतक साहेब 2025 के लिए रखे गए 2 पाठों के समाप्ति पूजन (14 जुलाई) पर श्री प्राणनाथ जी वाणी ज़ूम (प्रेम मंदिर) में पूज्य स्वामी जी के पधारने पर सभी सुंदरसाथ जी में उत्साह की लहर",
+        image: "/assets/img/bitaksaheb/14_day_2.jpg",
+        link: "https://youtu.be/lyT301B2Nng?si=Zv0TbuaecCgzmz2x",
+        bitakSandesh: {
+            text: "️पूज्य स्वामी जी के पधारने पर सभी सुंदरसाथ जी में उत्साह की लहर",
+            image: "/assets/img/bitaksaheb/14_day_1.jpg",
+        },
+        quote: {
+            text: "ध्यान, वाणी मन्थन और सेवा ही सुन्दरसाथ की आत्मिक सम्पति है।",
+            author: "श्री राजन स्वामी जी"
+        },
+        qa: {
+            question: "",
+            answer: "",
+            link: ""
+        },
+        game: {
+            title: "",
+            description: "",
+            link: ""
+        },
+        reel: {
+            youtube_embed: "MajSGbv7iic",
+            facebook_embed: "",
+            title: " स्वामी जी की ओर से सभी प्यारे सुंदरसाथ जी को प्रेम भरा निमंत्रण",
+            subtext: "श्री प्राणनाथ ज्ञानपीठ के 19वें वार्षिकोत्सव के लिए स्वामी जी की ओर से सभी प्यारे सुंदरसाथ जी को प्रेम भरा निमंत्रण एवं श्री बीतक साहेब चर्चा के प्रथम प्रयास के लिए पूज्य स्वामी जी का आशीर्वाद व शुभकामनाएँ"
+        }
+    },
+    {
+        id: 2,
         date: { day: "15", month: "July", year: "2025" },
         title: "अथ तीन सरूपों की बीतक",
         speaker: "सोनिया सखी (ऑस्ट्रेलिया)",
@@ -9,7 +43,7 @@ var bitakSahebList = [
         description: "अथ तीन सरूपों की बीतक — जीव, माया और ब्रह्म का गूढ़ ज्ञान, श्रद्धा और भक्ति से ओतप्रोत पहली दिन की अद्भुत शुरुआत।",
         image: "/assets/img/bitaksaheb/bitaksaheb_day1.png",
         link: "https://youtu.be/P2S20KesJhI?si=3p9J2L4A5WCRe0HB",
-        bitakSandesh : {
+        bitakSandesh: {
             text: "️सुनो नहीं, जी लो श्री बीतक साहेब का हर पल",
             image: "/assets/img/bitaksaheb/quotes/quote_message_day1.jpeg",
         },
@@ -28,13 +62,14 @@ var bitakSahebList = [
             link: "https://wordwall.net/resource/94832823"
         },
         reel: {
-            youtube_embed: "MajSGbv7iic",
-            title: " स्वामी जी की ओर से सभी प्यारे सुंदरसाथ जी को प्रेम भरा निमंत्रण",
-            subtext: "श्री प्राणनाथ ज्ञानपीठ के 19वें वार्षिकोत्सव के लिए स्वामी जी की ओर से सभी प्यारे सुंदरसाथ जी को प्रेम भरा निमंत्रण एवं श्री बीतक साहेब चर्चा के प्रथम प्रयास के लिए पूज्य स्वामी जी का आशीर्वाद व शुभकामनाएँ"
+            youtube_embed: "Zv3tysn1Y4U",
+            facebook_embed: "",
+            title: "#2 बीतक - महारास की रामतों में जागनी रास के संकेत",
+            subtext: ""
         }
     },
     {
-        id: 2,
+        id: 3,
         date: { day: "16", month: "July", year: "2025" },
         title: "आखिर क्यों रचा गया ब्रज और रास का खेल?",
         speaker: "सोनिया सखी (ऑस्ट्रेलिया)",
@@ -42,7 +77,7 @@ var bitakSahebList = [
         description: "🌹 Day 2 || सोनिया जी (ऑस्ट्रेलिया) द्वारा श्री बीतक चर्चा 2025 || Live Streaming- 16 July 🌹",
         image: "/assets/img/bitaksaheb/bitaksaheb_day2.png",
         link: "https://www.youtube.com/live/8RYneqG7DzU?si=rmCbftf0svEZTvfP",
-        bitakSandesh : {
+        bitakSandesh: {
             text: "️💎 श्री बीतक संदेश - 2, सुनो नहीं, जी लो — बीतक साहेब का हर पल 💎",
             image: "/assets/img/bitaksaheb/quotes/quote_message_day2.jpeg",
         },
@@ -53,16 +88,51 @@ var bitakSahebList = [
         qa: {
             question: "श्री बीतक क्विज़",
             answer: "SPJV श्री बीतक साहेब <br> Quiz Day 2",
-            link: "https://wordwall.net/play/94897/664/664"
+            link: "https://forms.gle/1MkmAZTecGGZbSES7"
         },
         game: {
             title: "ज्ञानवर्धक खेल",
             description: "अपना ज्ञान परखें इस मजेदार खेल के साथ!",
-            link: "https://forms.gle/1MkmAZTecGGZbSES7"
+            link: "https://wordwall.net/play/94897/664/664"
         },
         reel: {
-            youtube_embed: "Zv3tysn1Y4U",
-            title: " #1 बीतक – माया की आंधियों में श्रीजी का प्रेम",
+            youtube_embed: "MeWPaxUsP9Q",
+            facebook_embed: "",
+            title: "#2 बीतक - महारास की रामतों में जागनी रास के संकेत",
+            subtext: ""
+        }
+    },
+    {
+        id: 4,
+        date: { day: "17", month: "July", year: "2025" },
+        title: "प्रियतम की खोज में तप और त्याग की मूरत – श्री देवचन्द्र जी",
+        speaker: "सोनिया सखी (ऑस्ट्रेलिया)",
+        time: "6:00-7:30 am",
+        description: "प्रियतम की खोज में तप और त्याग की मूरत",
+        image: "/assets/img/bitaksaheb/bitaksaheb_day3.png",
+        link: "https://youtu.be/34fkae_prYU?si=_28hDaNEGnRidxUJ",
+        bitakSandesh: {
+            text: "️💎 श्री बीतक संदेश - 2, सुनो नहीं, जी लो — बीतक साहेब का हर पल 💎",
+            image: "",
+        },
+        quote: {
+            text: "जब जनमे मारवाड़ में, घर अति आनन्द नरनार। <br> यह बधाई ब्रह्मांड में, त्रिगुण समेत विस्तार।।",
+            author: "श्री प्राणनाथ जी"
+        },
+        qa: {
+            question: "श्री बीतक क्विज़",
+            answer: "SPJV श्री बीतक साहेब <br> Quiz Day 3",
+            link: ""
+        },
+        game: {
+            title: "ज्ञानवर्धक खेल",
+            description: "अपना ज्ञान परखें इस मजेदार खेल के साथ!",
+            link: "https://forms.gle/WBJgdJFbhZPRn9Wu6"
+        },
+        reel: {
+            youtube_embed: "",
+            facebook_embed: "",
+            title: "#2 बीतक - महारास की रामतों में जागनी रास के संकेत",
             subtext: ""
         }
     }
@@ -73,7 +143,6 @@ function displaykarkhanaNames(divid, bitakSaheb) {
     const studentsList = document.getElementById(divid);
 
     for (let i = 0; i < bitakSaheb.length; i++) {
-
         studentsList.innerHTML =
             studentsList.innerHTML +
             `
@@ -116,16 +185,15 @@ function displaykarkhanaNames(divid, bitakSaheb) {
                                     ${bitakSaheb[i].description}
                                 </p>
                                 <div class="row">
-                                    
-                                    <div class="col-sm-6">
+                                    <div class="col-sm-6 ${bitakSaheb[i].qa && bitakSaheb[i].qa.link.trim() !== '' ? '' : 'd-none'}">
                                         <div class="qa-box">
                                             <i data-feather="help-circle" class="mb-2 "></i><br>
-                                            <div class="question">${bitakSaheb[i].qa.question}</div>
+                                            <div class="question">${bitakSaheb[i].qa.question}  </div>
                                             <div class="answer">${bitakSaheb[i].qa.answer}</div>
                                             <a target="_blank" class="btn btn-primary pulse-btn-sm " href="${bitakSaheb[i].qa.link}">अभी क्लिक करें</a> 
                                         </div>
                                     </div>
-                                    <div class="col-sm-6">
+                                    <div class="col-sm-6 ${bitakSaheb[i].qa && bitakSaheb[i].game.link.trim() !== '' ? '' : 'd-none'}">
                                         <div class="game-card">
                                             <i data-feather="github" class="mb-2 "></i><br>
                                             <div class="game-title">${bitakSaheb[i].game.title}</div>
@@ -134,7 +202,7 @@ function displaykarkhanaNames(divid, bitakSaheb) {
                                                 class="btn btn-primary pulse-btn-sm  "> अभी खेलें</a>
                                         </div>
                                     </div>
-                                    <div class="col-sm-6">
+                                    <div class="col-sm-6 ${bitakSaheb[i].qa && bitakSaheb[i].quote.text.trim() !== '' ? '' : 'd-none'}">
                                         <div class="quote-glass">
                                             <div class="quote-text">
                                                 <i data-feather="message-circle"
@@ -144,7 +212,7 @@ function displaykarkhanaNames(divid, bitakSaheb) {
                                             <div class="quote-author">– ${bitakSaheb[i].quote.author}</div>
                                         </div>
                                     </div>
-                                    <div class="col-sm-6">
+                                    <div class="col-sm-6 ${bitakSaheb[i].qa && bitakSaheb[i].bitakSandesh.image.trim() !== '' ? '' : 'd-none'}">
                                         <div class="bitakSandesh-card">
                                             <img src="${bitakSaheb[i].bitakSandesh.image}"
                                             alt="Event Image" class="bitakSandesh-image mb-4">
@@ -153,13 +221,21 @@ function displaykarkhanaNames(divid, bitakSaheb) {
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-md-4 ${bitakSaheb[i].qa && bitakSaheb[i].reel.youtube_embed.trim() !== '' ? '' : 'd-none'}">
                                 <div class="reel-video">
                                     <iframe
                                         src="https://www.youtube.com/embed/${bitakSaheb[i].reel.youtube_embed}?autoplay=0&mute=1&controls=1&&modestbranding=0&rel=0&loop=1"
                                         title="YouTube Reel"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                         allowfullscreen></iframe>
+                                    <h2 class="reel-video-title">${bitakSaheb[i].reel.title}</h2>
+                                    <p class="bitak-text text-center">${bitakSaheb[i].reel.subtext}</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4 ${bitakSaheb[i].qa && bitakSaheb[i].reel.facebook_embed.trim() !== '' ? '' : 'd-none'}">
+                                <div class="reel-video">
+                                <iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1378896653223910%2F&show_text=false&width=476&t=0" width="476" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                                   
                                     <h2 class="reel-video-title">${bitakSaheb[i].reel.title}</h2>
                                     <p class="bitak-text text-center">${bitakSaheb[i].reel.subtext}</p>
                                 </div>
