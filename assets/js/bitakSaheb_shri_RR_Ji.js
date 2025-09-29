8114a164f800570810e23e77bf372a1e55eed304
ashish0038 <ashish.0038@gmail.com>
2025-09-29 20:35:40 +0530

    added shri ram ratandas ji maharaj ji bitak page

---
+++ assets/js/bitakSaheb_shri_RR_Ji.js
@@ -0,0 +1,265 @@
+var bitakSahebListShriRRji = [
+    {
+        id: 1,
+        date: { day: "29", month: "September", year: "2025" },
+        quote: {
+            text: "️",
+            image: "/assets/img/bitaksahebshriRRDji/prem_pati_1.jpeg",
+        },
+        qa: {
+            question:  "ज्ञानवर्धक क्विज़",
+            answer: "परमहंस महाराज श्री रामरतन दास जी की क्विज़ - 1",
+            link: "https://forms.gle/BggF3pUf9esEovVR9"
+        },
+        game: {
+            title: "ज्ञानवर्धक गेम्स",
+            description: "परमहंस महाराज श्री रामरतन दास जी की गेम्स - 1",
+            link: "https://wordwall.net/play/97389/567/206"
+        },
+        reel: {
+            youtube_embed: "XlW3K8kozI4",
+            facebook_embed: "",
+            title: "परमहंस महाराज श्री रामरतन दास जी की बीतक रील 1",
+            subtext: ""
+        }
+    },
+    {
+        id: 2,
+        date: { day: "29", month: "September", year: "2025" },
+        quote: {
+            text: "️",
+            image: "/assets/img/bitaksahebshriRRDji/prem_pati_2.jpeg",
+        },
+        qa: {
+            question:  "ज्ञानवर्धक क्विज़",
+            answer: "परमहंस महाराज श्री रामरतन दास जी की क्विज़ - 2",
+            link: "https://forms.gle/CamdNfXaJabELh5x8"
+        },
+        game: {
+            title: "ज्ञानवर्धक गेम्स",
+            description: "परमहंस महाराज श्री रामरतन दास जी की गेम्स - 2",
+            link: ""
+        },
+        reel: {
+            youtube_embed: "y6abfjraXx4",
+            facebook_embed: "",
+            title: "परमहंस महाराज श्री रामरतन दास जी की बीतक रील 2",
+            subtext: ""
+        }
+    },
+    {
+        id: 3,
+        date: { day: "29", month: "September", year: "2025" },
+        quote: {
+            text: "️",
+            image: "/assets/img/bitaksahebshriRRDji/prem_pati_3.jpeg",
+        },
+        qa: {
+            question:  "ज्ञानवर्धक क्विज़",
+            answer: "परमहंस महाराज श्री रामरतन दास जी की क्विज़ - 3",
+            link: ""
+        },
+        game: {
+            title: "ज्ञानवर्धक गेम्स",
+            description: "परमहंस महाराज श्री रामरतन दास जी की गेम्स - 3",
+            link: ""
+        },
+        reel: {
+            youtube_embed: "X_4489xa0BA",
+            facebook_embed: "",
+            title: "परमहंस महाराज श्री रामरतन दास जी की बीतक रील 3",
+            subtext: ""
+        }
+    },
+    {
+        id: 4,
+        date: { day: "29", month: "September", year: "2025" },
+        quote: {
+            text: "️",
+            image: "/assets/img/bitaksahebshriRRDji/prem_pati_4.jpeg",
+        },
+        qa: {
+            question:  "ज्ञानवर्धक क्विज़",
+            answer: "परमहंस महाराज श्री रामरतन दास जी की क्विज़ - 4",
+            link: ""
+        },
+        game: {
+            title: "ज्ञानवर्धक गेम्स",
+            description: "परमहंस महाराज श्री रामरतन दास जी की गेम्स - 4",
+            link: ""
+        },
+        reel: {
+            youtube_embed: "5TKE5hfRv70",
+            facebook_embed: "",
+            title: "परमहंस महाराज श्री रामरतन दास जी की बीतक रील 4",
+            subtext: ""
+        }
+    },
+    {
+        id: 5,
+        date: { day: "29", month: "September", year: "2025" },
+        quote: {
+            text: "️",
+            image: "",
+        },
+        qa: {
+            question:  "ज्ञानवर्धक क्विज़",
+            answer: "परमहंस महाराज श्री रामरतन दास जी की क्विज़ - 5",
+            link: ""
+        },
+        game: {
+            title: "ज्ञानवर्धक गेम्स",
+            description: "परमहंस महाराज श्री रामरतन दास जी की गेम्स - 5",
+            link: ""
+        },
+        reel: {
+            youtube_embed: "15J0S9R3Kjk",
+            facebook_embed: "",
+            title: "परमहंस महाराज श्री रामरतन दास जी की बीतक रील 5",
+            subtext: ""
+        }
+    },
+    {
+        id: 6,
+        date: { day: "29", month: "September", year: "2025" },
+        quote: {
+            text: "️",
+            image: "",
+        },
+        qa: {
+            question:  "ज्ञानवर्धक क्विज़",
+            answer: "परमहंस महाराज श्री रामरतन दास जी की क्विज़ - 6",
+            link: ""
+        },
+        game: {
+            title: "ज्ञानवर्धक गेम्स",
+            description: "परमहंस महाराज श्री रामरतन दास जी की गेम्स - 6",
+            link: ""
+        },
+        reel: {
+            youtube_embed: "T_ZlIpDb91E",
+            facebook_embed: "",
+            title: "परमहंस महाराज श्री रामरतन दास जी की बीतक रील 6",
+            subtext: ""
+        }
+    },
+    {
+        id: 7,
+        date: { day: "29", month: "September", year: "2025" },
+        quote: {
+            text: "️",
+            image: "",
+        },
+        qa: {
+            question:  "ज्ञानवर्धक क्विज़",
+            answer: "परमहंस महाराज श्री रामरतन दास जी की क्विज़ - 7",
+            link: ""
+        },
+        game: {
+            title: "ज्ञानवर्धक गेम्स",
+            description: "परमहंस महाराज श्री रामरतन दास जी की गेम्स - 7",
+            link: ""
+        },
+        reel: {
+            youtube_embed: "Y5O-PCGgWGE",
+            facebook_embed: "",
+            title: "परमहंस महाराज श्री रामरतन दास जी की बीतक रील 7",
+            subtext: ""
+        }
+    }
+]
+
+function displayReelsList(divid, bitakSaheb) {
+    const studentsList = document.getElementById(divid);
+
+    for (let i = 0; i < bitakSaheb.length; i++) {
+        studentsList.innerHTML =
+            studentsList.innerHTML +
+            `
+                <div class="col-md-3 ${bitakSaheb[i].qa && bitakSaheb[i].reel.youtube_embed.trim() !== '' ? '' : 'd-none'}">
+                    <div class="reel-video">
+                        <iframe style="height: 540px;"
+                            src="https://www.youtube.com/embed/${bitakSaheb[i].reel.youtube_embed}?autoplay=0&mute=1&controls=1&&modestbranding=0&rel=0&loop=1"
+                            title="YouTube Reel"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                        <h2 class="reel-video-title">${bitakSaheb[i].reel.title}</h2>
+                        <p class="bitak-text text-center">${bitakSaheb[i].reel.subtext}</p>
+                    </div>
+                </div>
+                <div class="col-md-3 ${bitakSaheb[i].qa && bitakSaheb[i].reel.facebook_embed.trim() !== '' ? '' : 'd-none'}">
+                    <div class="reel-video">
+                        <div class="fb-video" style="height: 540px;" data-href="https://www.facebook.com/reel/${bitakSaheb[i].qa && bitakSaheb[i].reel.facebook_embed}"  data-width="500"  data-show-text="false"></div>    
+                        <h2 class="reel-video-title">${bitakSaheb[i].reel.title}</h2>
+                        <p class="bitak-text text-center">${bitakSaheb[i].reel.subtext}</p>
+                    </div>
+                </div>
+            `;
+    }
+}
+
+function displayQuizeList(divid, bitakSaheb) {
+    const studentsList = document.getElementById(divid);
+
+    for (let i = 0; i < bitakSaheb.length; i++) {
+        studentsList.innerHTML =
+            studentsList.innerHTML +
+            `
+            <div class="col-sm-3 ${bitakSaheb[i].qa && bitakSaheb[i].qa.link.trim() !== '' ? '' : 'd-none'}">
+                <div class="qa-box">
+                    <i data-feather="help-circle" class="mb-2 "></i><br>
+                    <div class="question">${bitakSaheb[i].qa.question}  </div>
+                    <div class="game-desc">${bitakSaheb[i].qa.answer}</div>
+                    <a target="_blank" class="btn btn-primary pulse-btn-sm " href="${bitakSaheb[i].qa.link}">बीतक क्विज़</a> 
+                </div>
+            </div>
+            `;
+    }
+}
+
+function displayGamesList(divid, bitakSaheb) {
+    const studentsList = document.getElementById(divid);
+
+    for (let i = 0; i < bitakSaheb.length; i++) {
+        studentsList.innerHTML =
+            studentsList.innerHTML +
+            `
+            <div class="col-sm-3 ${bitakSaheb[i].qa && bitakSaheb[i].game.link.trim() !== '' ? '' : 'd-none'}">
+                <div class="game-card">
+                    <i data-feather="github" class="mb-2 "></i><br>
+                    <div class="game-title">${bitakSaheb[i].game.title}</div>
+                    <div class="game-desc">${bitakSaheb[i].game.description}</div>
+                    <a href="${bitakSaheb[i].game.link}" target="_blank"
+                        class="btn btn-primary pulse-btn-sm  "> बीतक गेम्स खेलें</a>
+                </div>
+            </div>
+            `;
+    }
+}
+
+function displayQuotesList(divid, bitakSaheb) {
+    const studentsList = document.getElementById(divid);
+
+    for (let i = 0; i < bitakSaheb.length; i++) {
+        studentsList.innerHTML =
+            studentsList.innerHTML +
+            `
+            <div class="col-sm-3 ${bitakSaheb[i].qa && bitakSaheb[i].quote.image.trim() !== '' ? '' : 'd-none'}">
+                <div class="bitakSandesh-card">
+                    <img src="${bitakSaheb[i].quote.image}"
+                    alt="Event Image" class="bitakSandesh-image mb-4">
+                    <h2 class="bitakSandesh-title">${bitakSaheb[i].quote.text}</h2>
+                </div>
+            </div>
+            `;
+    }
+}
+
+
+
+displayReelsList("displayReelsList", bitakSahebListShriRRji);
+displayQuizeList("displayQuizeList", bitakSahebListShriRRji);
+displayQuotesList("displayQuotesList", bitakSahebListShriRRji);
+displayGamesList("displayGamesList", bitakSahebListShriRRji);
+
+
